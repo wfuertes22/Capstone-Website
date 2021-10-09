2c833d68f2e7f18b0f8014213838ac43bcb0ff13
cpociask <cmp557@nau.edu>
2021-10-08 19:58:23 -0700

    Bigger, more spaced out, and clickable title. Moved team page from separate file to tab. Added temporary font.

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function openTab(tabName) {
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";  
     }
-    document.getElementById(tabName).style.display = "block";  
+    document.getElementById(tabName).style.display = "block";
 }
 
 var x = document.getElementsByClassName("tab-head");
 for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
 }
